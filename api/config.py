532cfde8419a9f078ef7db8dad8a6173c3d98ef0
BAEK <highcloud100@inha.edu>
2022-05-10 11:30:01 +0900

    emo_ubuntu

--- api/config.py
+++ api/config.py
@@ -1,9 +1,5 @@
 db = {
-  'user' : 'movie',
-  'password' : 'emo123movie?',
-  'host': 'localhost',
-  'port' : 3306,
-  'database' : 'emo_movie'
+
 }
 
 DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
