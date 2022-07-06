73bfe6046e28f5ffce159641269c9ee8dce47d68
Baek <highcloud100@inha.edu>
2022-07-06 11:14:51 +0900

    logging

--- api/config/product.py
+++ api/config/product.py
@@ -1,3 +1,7 @@
+from logging.config import dictConfig
+import os
+
+
 db = {
       'user' : 'emo',
       'password' : 'EMOmovie123!',
@@ -10,3 +14,29 @@ DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db
 
 
 SECRET_KEY = b'\xd1]0 CV\x9a\rB`}\x98\xa2z3\xca'
+
+os.chdir("../")
+BASE_DIR = os.getcwd()
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
